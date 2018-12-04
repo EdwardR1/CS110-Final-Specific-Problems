9e0ee6dc8c55d7c4c2bb9e2f5f4d91e44c5589eb
Edward Rees <erees.hk@gmail.com>
2018-12-04 13:22:54 -0800

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,5 +1,5 @@
 # In a list of the following names:
-    # John, Steve, Joe, Fred
+# John, Steve, Joe, Fred
 # Print out all the names
 # Search for a specific name in a function, that has the list and a name as a parameter
 # Function to add a new name to the list
@@ -8,3 +8,10 @@
 # Create a function, searching for a name at a specific index (function takes in two parameters, the list itself and the index)
 # If no error, return the name
 # If error caught, return “Index is out of bounds, input a valid index"
+
+
+# create a 2D list of all the outcomes of two dice rolling
+def dice():
+    list = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+    return list
